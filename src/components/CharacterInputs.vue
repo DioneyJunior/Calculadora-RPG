c9c28c5b5297a5533ff7f1e5b2522888d4183418
Dioney Junior <dioneyfbjunior@gmail.com>
2023-04-03 13:49:07 -0300

    add navbar

---
+++ src/components/CharacterInputs.vue
@@ -0,0 +1,114 @@
+<template>
+        <!-- Personagem -->
+        <div
+      style="background-color: #93baca; overflow: auto"
+      class="offcanvas offcanvas-start show"
+      tabindex="-1"
+      id="offcanvas"
+      aria-labelledby="offcanvasLabel"
+    >
+      <div class="offcanvas-header">
+        <h5 class="offcanvas-title" id="offcanvasLabel">Ficha de Personagem</h5>
+        <button
+          type="button"
+          class="btn-close"
+          data-bs-dismiss="offcanvas"
+          aria-label="Close"
+        ></button>
+      </div>
+      <div class="container-fluid" id="personagem">
+        <label for="name">Nome</label>
+        <input
+          placeholder="Insira o nome"
+          class="form-control"
+          type="text"
+          name=""
+          id="name"
+        />
+        <br />
+
+        <label for="strInput" class="mb-2">
+          Força*
+        </label>
+        <input
+          placeholder="Modificador de força Ex: 3"
+          class="form-control"
+          type="number"
+          id="strInput"
+        />
+        <br />
+
+        <label for="constInput" class="mb-2">
+          Constituição*
+        </label>
+        <input
+          placeholder="Modificador de Constituição Ex: 3"
+          class="form-control"
+          type="number"
+          id="constInput"
+        />
+        <br />
+
+        <label for="dexInput" class="mb-2">
+          Destreza*
+        </label>
+        <input
+          placeholder="Modificador de Destreza Ex: 3"
+          class="form-control"
+          type="number"
+          id="dexInput"
+        />
+        <br />
+
+        <label for="intInput" class="mb-2">
+          Inteligência*
+        </label>
+        <input
+          placeholder="Modificador de Inteligência Ex: 3"
+          class="form-control"
+          type="number"
+          id="intInput"
+        />
+        <br />
+
+        <label for="wisInput" class="mb-2">
+          Sabedoria*
+        </label>
+        <input
+          placeholder="Modificador de Sabedoria Ex: 3"
+          class="form-control"
+          type="number"
+          id="wisInput"
+        />
+        <br />
+
+        <label for="charInput" class="mb-2">
+          Carisma*
+        </label>
+        <input
+          placeholder="Modificador de Carisma Ex: 3"
+          class="form-control"
+          type="number"
+          id="charInput"
+        /><br />
+
+        <label for="profInput" class="mb-2">
+          Bonus de proficiência*
+        </label>
+        <input
+          placeholder="Bonus de proficiência Ex: 3"
+          class="form-control"
+          type="number"
+          id="profInput"
+        />
+        <br />
+        <button
+          type="button"
+          class="btn btn-dark"
+          data-bs-dismiss="offcanvas"
+        >
+          Salvar Personagem
+        </button>
+      </div>
+    </div>
+</template>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,43 @@
+<template>
+<nav class="navbar navbar-expand-lg bg-body-tertiary w-100">
+      <div class="container-fluid">
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="offcanvas"
+          data-bs-target="#offcanvas"
+          aria-controls="offcanvas"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div class="navbar-nav">
+            <button
+              class="btn btn-primary"
+              type="button"
+              data-bs-toggle="offcanvas"
+              data-bs-target="#offcanvas"
+              aria-controls="offcanvas"
+              id="btn-ficha"
+            >
+              <i class="bi bi-person-fill"></i>
+              Ficha de Personagem
+            </button>
+          </div>
+        </div>
+      </div>
+    </nav>
+    <CharacterInputs />
+</template>
+<style scoped>
+nav{
+    background-color:#1f74be
+}
+</style>
+<script>
+import CharacterInputs from './CharacterInputs.vue';
+export default {
+    name: 'NavBar',
+    components: { CharacterInputs },
+      }
+</script>
