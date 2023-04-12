50ea8c8fe7d1be127bcaa75021d51a1c38ec4b3e
Dioney Junior <dioneyfbjunior@gmail.com>
2023-04-12 05:20:30 -0300

    navbar fix

--- src/App.vue
+++ src/App.vue
@@ -3,50 +3,24 @@
   <header>
     <nav class="navbar navbar-expand-lg p-0" id="navbar">
       <div>
-        <button
-        class="navbar-toggler"
-        type="button"
-        data-bs-toggle="offcanvas"
-        data-bs-target="#offcanvas"
-        aria-controls="offcanvas"
-        >
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <div class="navbar-nav">
-          <button
-          class="btn btn-primary"
-          type="button"
-          data-bs-toggle="offcanvas"
-          data-bs-target="#offcanvas"
-          aria-controls="offcanvas"
-          id="btn-ficha"
-          >
-          <i class="bi bi-person-fill"></i>
-          Ficha de Personagem
+        <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvas"
+          aria-controls="offcanvas">
+          <span class="navbar-toggler-icon"></span>
         </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div class="navbar-nav">
+            <button class="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvas"
+              aria-controls="offcanvas" id="btn-ficha">
+              <i class="bi bi-person-fill"></i>
+              Ficha de Personagem
+            </button>
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
-</nav>
-<CharacterInputs />
-</header>
-<br />
-<div class="col-12 py-3">
-  <div class="col-12 py-3">
-    <div class="row p-0 m-0">
-      <div class="col-12 text-center">
-        <form>
-          <input type="radio" v-model="selectedDice" value="d4" id="d4" />
-          <label for="d4">
-            d4<img src="./components/img/d4.png" class="img d-block" id="d4img"
-              />
-          </label>
-          
-          <input type="radio" v-model="selectedDice" value="d6" id="d6" />
-          <label for="d6">
-            d6<img src="./components/img/d6.png" class="img d-block" id="d6img"
-              />
+    </nav>
+    <CharacterInputs />
+  </header>
+  <br />
             </label>
             
             <input type="radio" v-model="selectedDice" value="d8" id="d8"/>
@@ -102,23 +76,21 @@
   </button>
 </div>
 
-<Style>
-#navbar {
-  position: fixed;
-  top: 0;
-  width: 100%;
-  width: 100%;
-  background-color: #484747;
-  border-bottom: 5px solid #000000;
-}
-@media (max-width: 768px) {
-  .navbar #btn-ficha {
-    display: none;
-  }
-}
-body {
-  width: 100%
-}
+  <Style>
+    nav {
+      position: fixed;
+      bottom: 0;
+      width: 100%;
+      background-color: #484747;
+      border-bottom: 5px solid #000000;
+    }
+
+    @media (max-width: 768px) {
+      .navbar #btn-ficha {
+        display: none;
+      }
+    }
+
 
 </Style>
 </template>
